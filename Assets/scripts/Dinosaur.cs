24eeae50ef6fdd3d029738bf4c40c33250ba98df
Swackles <svenerik.rebane@gmail.com>
2020-12-29 21:26:57 +0200

    #16 Dinosaur Prefab

---
+++ Assets/scripts/Dinosaur.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using QFSW.QC;
+
+[CommandPrefix("Dinosaur")]
+public class Dinosaur : MonoBehaviour
+{
+    public void Idle()
+    {
+        Animator animator = GetComponent<Animator>();
+        animator.SetBool("Howl", false);
+        animator.SetBool("Scream", false);
+    }
+    [Command]
+    public void Howl()
+    {
+        Idle();
+        GetComponent<Animator>().SetBool("Howl", true);
+    }
+
+    [Command]
+    public void Scream()
+    {
+        Idle();
+        GetComponent<Animator>().SetBool("Scream", true);
+    }
+}
