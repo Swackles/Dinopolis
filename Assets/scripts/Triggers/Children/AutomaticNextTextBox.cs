d510b6480aba95a6e129752ef4fb95ed82d470ca
Swackles <svenerik.rebane@gmail.com>
2020-12-29 22:16:12 +0200

    Added Respond scene

---
+++ Assets/scripts/Triggers/Children/AutomaticNextTextBox.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Triggers.Children
+{
+    class AutomaticNextTextBox : TextBox, IChild
+    {
+        new public void Action()
+        {
+            base.Action();
+
+            StartCoroutine(WaitForFinish());
+        }
+
+        private IEnumerator WaitForFinish()
+        {
+            while (!this.Finished())
+                yield return new WaitForSeconds(0.1f);
+
+            yield return new WaitForSeconds(2f);
+            GetComponentInParent<Container>().Next();
+        }
+    }
+}
