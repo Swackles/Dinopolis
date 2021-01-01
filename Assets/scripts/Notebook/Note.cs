216085ef463a5e0d82aec4ae0d87329ab576822b
Swackles <svenerik.rebane@gmail.com>
2021-01-01 20:07:21 +0200

    #17 Created notebook prefab

---
+++ Assets/scripts/Notebook/Note.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using QFSW.QC;
+
+namespace Notebook
+{
+    enum MoveState
+    {
+        Stand,
+        Shrink, // Shrink the Note
+        Expand // Expand the Note
+    }
+
+    [CommandPrefix("Notebook.Note.")]
+    class Note : MonoBehaviour, IPointerClickHandler
+    {
+        Vector3 OriginalPosition;
+        Vector3 ExpandedPosition;
+        Vector3 OriginalScale;
+        Vector3 ExpandedScale;
+        float speed = 1000;
+        float scaleSpeed = 1000;
+        MoveState State = MoveState.Stand;
+        public bool Highlighted = false;
+
+        public void Awake()
+        {
+            OriginalPosition = transform.position;
+            ExpandedPosition = GetComponentInParent<NotebookContainer>().Center.gameObject.transform.position;
+
+            OriginalScale = transform.localScale;
+            ExpandedScale = GetComponentInParent<NotebookContainer>().Center.gameObject.transform.localScale;
+        }
+
+        public void Update()
+        {
+            if (transform.hasChanged && State == MoveState.Stand && transform.position != ExpandedPosition)
+                OriginalPosition = transform.position;
+
+            if (State == MoveState.Shrink)
+                Move(OriginalPosition, OriginalScale);
+            else if (State == MoveState.Expand)                
+                Move(ExpandedPosition, ExpandedScale);
+        }
+
+        public void OnPointerClick(PointerEventData pointer) {
+            if (transform.position == OriginalPosition && transform.localScale == OriginalScale)
+                Expand();
+        }
+
+        private void Move(Vector3 MoveTo, Vector3 ScaleTo)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, MoveTo, speed * Time.deltaTime);
+            transform.localScale = Vector3.MoveTowards(transform.localScale, ScaleTo, scaleSpeed * Time.deltaTime);
+
+            if (transform.position == MoveTo && transform.localScale == ScaleTo)
+            {
+                if (MoveTo == ExpandedPosition && ScaleTo == ExpandedScale)
+                {
+                    GetComponentInParent<NotebookContainer>().Submit.SetActive(true);
+                    Highlighted = true;
+                } else
+                {
+                    Highlighted = false;
+                }
+                    
+
+                State = MoveState.Stand;
+            }
+        }
+
+        [Command]
+        public void Expand()
+        {
+            State = MoveState.Expand;
+        }
+
+        [Command]
+        public void Shrink()
+        {
+            State = MoveState.Shrink;
+        }
+    }
+}
---
+++ Assets/scripts/Notebook/NotebookContainer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+namespace Notebook
+{
+    class NotebookContainer : MonoBehaviour
+    {
+        [SerializeField]
+        GameObject Background = null;
+        [SerializeField]
+        public GameObject Center = null;
+        [SerializeField]
+        public GameObject Submit = null;
+
+        public void ShowHide()
+        {
+            Background.SetActive(!Background.activeSelf);
+        }
+
+        public void CloseHighlighted()
+        {
+            Note Highlighted = GetComponentsInChildren<Note>().FirstOrDefault(x => { return x.Highlighted; });
+
+            Highlighted.Shrink();
+            Submit.SetActive(false);
+        }
+    }
+}
